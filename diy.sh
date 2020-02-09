1ea88f704bbac5226e339c4ad5abac6b21e91a9a
m0e16 <58701558+m0e16@users.noreply.github.com>
2020-02-09 13:04:13 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,10 +8,10 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #移植R1S代码
-#git clone https://github.com/jerrykuku/openwrt-nanopi-r1s-h5 temporary
-#cp -rf temporary/* .
-#rm -rf temporary
-#chmod 0755 target/linux/sunxi/base-files/etc/board.d/01_leds
+git clone https://github.com/jerrykuku/openwrt-nanopi-r1s-h5 temporary
+cp -rf temporary/* .
+rm -rf temporary
+chmod 0755 target/linux/sunxi/base-files/etc/board.d/01_leds
 #替换dnsmasq-full
 sed -i 's/dnsmasq /dnsmasq-full default-settings luci /' include/target.mk
 #更新主题
